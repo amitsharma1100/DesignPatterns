26d588a100f114481c2e64f094a864519d0305db
amitsharma1100 <sharma.amit1100@gmail.com>
2018-10-12 12:02:52 +0530

    Adapter Pattern

---
+++ DesignPatterns/Patterns/Adapter/ITarget.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.Patterns.Adapter
+{
+    public interface ITarget
+    {
+        List<string> GetEmployeeList();
+    }
+}
---
+++ DesignPatterns/Patterns/Adapter/ThirdPartyBillingSystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns.Patterns.Adapter
+{
+    public class ThirdPartyBillingSystem
+    {
+        private ITarget employeeSource;
+
+        public ThirdPartyBillingSystem(ITarget employeeSource)
+        {
+            this.employeeSource = employeeSource;
+        }
+
+        public void ShowEmployeeList()
+        {
+            List<string> employee = employeeSource.GetEmployeeList();
+
+            Console.WriteLine("######### Employee List ##########");
+            foreach (var item in employee)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
