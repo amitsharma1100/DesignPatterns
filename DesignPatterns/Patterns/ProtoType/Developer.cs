79fe50b9a526e10db55f432546c3b1337f5e1430
amitsharma1100 <sharma.amit1100@gmail.com>
2018-10-12 12:07:48 +0530

    ProtoType Pattern

---
+++ DesignPatterns/Patterns/ProtoType/Developer.cs
@@ -0,0 +1,23 @@
+namespace DesignPatterns.Patterns.ProtoType
+{
+    /// <summary>
+    /// A 'ConcretePrototype' class
+    /// </summary>
+    public class Developer : IEmployee
+    {
+        public int WordsPerMinute { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public string PreferredLanguage { get; set; }
+
+        public IEmployee Clone()
+        {
+            return (IEmployee)MemberwiseClone();
+        }
+
+        public string GetDetails()
+        {
+            return string.Format("{0} - {1} - {2}", Name, Role, PreferredLanguage);
+        }
+    }
+}
